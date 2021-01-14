2a1ad574d914b733359cebbf555dede02f5f06fc
BenoitLardinois <benoitlardinois@hotmail.com>
2021-01-14 18:52:09 +0300

    first_commit

---
+++ my_ui_kit/test/controllers/ui_controller_test.rb
@@ -0,0 +1,13 @@
+require "test_helper"
+
+class UiControllerTest < ActionDispatch::IntegrationTest
+  test "should get new" do
+    get ui_new_url
+    assert_response :success
+  end
+
+  test "should get index" do
+    get ui_index_url
+    assert_response :success
+  end
+end
